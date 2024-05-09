69281688c345b3e1c8e0c340f5f0a7e5294026b6
TtMyth123 <TtMyth123@163.com>
2024-05-09 18:16:37 +0800

    TtMapCache

--- SyncMapCache/a_test.go
+++ SyncMapCache/a_test.go
@@ -16,9 +16,9 @@ func TestA(t *testing.T) {
 	a1 := T1{A1: 1000}
 	a2 := T1{A1: 2000}
 	aTtMapCache.SetCache("aa", a1, -1)
-	aa1, e := aTtMapCache.GetCache("aa", T1{})
+	aa1, e := aTtMapCache.GetCache("aa")
 
-	a2 = aTtMapCache.GetCacheEx("aa", T2{}).(T1)
+	a2 = aTtMapCache.GetCacheEx("aa").(T1)
 
 	fmt.Println(a2, e, aa1)
 
