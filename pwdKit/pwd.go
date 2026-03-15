192d9698c3530efed3164eb10e073a279f6fe97b
TtMyth123 <TtMyth123@163.com>
2026-03-15 18:16:46 +0800

    1.25 go 不支持1024位RSA密钥 所以添加支持1024位RSA方法

--- pwdKit/pwd.go
+++ pwdKit/pwd.go
@@ -3,13 +3,19 @@ package pwdKit
 import (
 	"crypto/hmac"
 	"crypto/md5"
+	"crypto/rsa"
 	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io"
+	"math/big"
+
+	"crypto/rand"
 )
 
 /*
@@ -52,10 +58,7 @@ func Md5ToStr(pwd string) string {
 	return newPwd
 }
 
-/*
-*
-标准将pwd字符串进行Md5加密并转换成string
-*/
+// StdMd5ToStr /*
 func StdMd5ToStr(pwd string) string {
 	md5data := md5.Sum([]byte(pwd))
 	str := fmt.Sprintf("%x", md5data)
@@ -87,4 +90,79 @@ func TtDecodeString(str, pwd string) (string, error) {
 	}
 	return "", errors.New("秘钥不对")
 
+}
+
+/*
+*
+1.25 go 不支持1024位RSA密钥 所以添加支持1024位RSA方法
+*/
+func RSAEncryptWith1024Key(plainText string, pubKeyPEM string) (string, error) {
+	// 解析 PEM
+	block, _ := pem.Decode([]byte(pubKeyPEM))
+	if block == nil {
+		return "", fmt.Errorf("failed to parse PEM block")
+	}
+
+	// 解析公钥
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse public key: %v", err)
+	}
+
+	rsaPub, ok := pub.(*rsa.PublicKey)
+	if !ok {
+		return "", fmt.Errorf("not an RSA public key")
+	}
+
+	fmt.Printf("使用 %d 位 RSA 密钥进行加密\n", rsaPub.N.BitLen())
+
+	// 使用自定义加密函数
+	ciphertext, err := customRSAEncryptPKCS1v15(rsaPub, []byte(plainText))
+	if err != nil {
+		return "", fmt.Errorf("encryption failed: %v", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+func customRSAEncryptPKCS1v15(pub *rsa.PublicKey, msg []byte) ([]byte, error) {
+	k := (pub.N.BitLen() + 7) / 8
+	if len(msg) > k-11 {
+		return nil, fmt.Errorf("message too long")
+	}
+
+	// 构造 EM = 0x00 || 0x02 || PS || 0x00 || M
+	em := make([]byte, k)
+	em[0] = 0x00
+	em[1] = 0x02
+
+	// 生成随机填充
+	psLen := k - len(msg) - 3
+	_, err := rand.Read(em[2 : 2+psLen])
+	if err != nil {
+		return nil, err
+	}
+
+	// 设置分隔符
+	em[2+psLen] = 0x00
+
+	// 复制消息
+	copy(em[3+psLen:], msg)
+
+	// 将 EM 转换为大整数
+	m := new(big.Int).SetBytes(em)
+
+	// 计算密文: c = m^e mod n
+	c := new(big.Int).Exp(m, big.NewInt(int64(pub.E)), pub.N)
+
+	// 将密文转换回字节数组
+	ciphertext := c.Bytes()
+
+	// 补齐到 k 字节
+	if len(ciphertext) < k {
+		padded := make([]byte, k)
+		copy(padded[k-len(ciphertext):], ciphertext)
+		ciphertext = padded
+	}
+
+	return ciphertext, nil
 }
--- pwdKit/pwd_tset.go
+++ pwdKit/pwd_tset.go
@@ -14,3 +14,23 @@ func TestMd5ToStr(t *testing.T) {
 	str1 := Md5ToStr(str)
 	fmt.Println(str1, str)
 }
+
+func Test_RSAEncryptWith1024Key(t *testing.T) {
+	PublicKey := `-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH1ufjmfY7tAt14i4z1ba8HvjCEf
+HeGdG8+9ABVbZhPphrW15dyb/Q6HfTCyBwDLdGZViiPcXli3/guMfv83o6nLZPyz
+gg8f7fblKF9vB5l9AmuzKSVL/8DCBaIXIr1aRmMBV04f1v4xXPyUdNXQkzDkXobX
+wPpk3jyb/K/XKefJAgMBAAE=
+-----END PUBLIC KEY-----
+`
+	password := "a"
+
+	encrypted, err := RSAEncryptWith1024Key(password, PublicKey)
+	if err != nil {
+		fmt.Printf("加密失败: %v\n", err)
+		return
+	}
+
+	fmt.Println("加密结果:", encrypted)
+
+}
